71f2cddc2b3e9bb641b126cb1e659437ff388737
Lamia <elbo.lamia@gmail.com>
2025-03-06 16:58:27 +0100

    Add files via upload

---
+++ lcbp_a-frontend/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production : false,
+    // urlBase: 'http://51.83.69.138:9002'
+    urlBase : 'http://localhost:81'
+};
---
+++ lcbp_a-frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production : true,
+    urlBase: 'http://51.83.69.138:9002'
+    // urlBase : 'http://localhost:81'
+};
